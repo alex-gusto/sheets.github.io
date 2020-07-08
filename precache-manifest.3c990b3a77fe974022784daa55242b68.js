a7185fbf15482d281d07f3dfbb4208adcede7dd0
Alex Mezhenskyi <oleksandr.mezhenskyi@agr-software.com>
2020-07-08 10:31:36 +0300

    Updates

---
+++ precache-manifest.3c990b3a77fe974022784daa55242b68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e23e0973177a1d1d7ce03f70440b723",
+    "url": "/sheets.github.io/index.html"
+  },
+  {
+    "revision": "a3619f60ee8b7432d35d",
+    "url": "/sheets.github.io/static/css/2.d8aa5323.chunk.css"
+  },
+  {
+    "revision": "87c9dcb4b4476a78ab78",
+    "url": "/sheets.github.io/static/css/main.8f5034c2.chunk.css"
+  },
+  {
+    "revision": "a3619f60ee8b7432d35d",
+    "url": "/sheets.github.io/static/js/2.220fcfab.chunk.js"
+  },
+  {
+    "revision": "9959b0da2a264037d4334930abc2790f",
+    "url": "/sheets.github.io/static/js/2.220fcfab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87c9dcb4b4476a78ab78",
+    "url": "/sheets.github.io/static/js/main.ea2c4bf2.chunk.js"
+  },
+  {
+    "revision": "84ba677c86c219dce2b9",
+    "url": "/sheets.github.io/static/js/runtime-main.d121ccbd.js"
+  }
+]);
